52484966b4a0d237b504d5905c4bb6d605b39159
sundelun <76630582+sundelun@users.noreply.github.com>
2023-08-20 23:55:31 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 def toGray():
     img = Image.open("images/beijing.jpg")
     
     grayscale_img = img.convert("L")
-    
     grayscale_img.save("images/output.jpg")
     
+    blur=img.filter(ImageFilter.BoxBlur(30))
+    blur.show()
+    
+def flip():
+    img=Image.open("images/beijing.jpg")
+    converted=img.transpose(Image.FLIP_TOP_BOTTOM)
+    converted.show()
+
 if __name__ == "__main__":
-    toGray()
+    #toGray()
+    flip()
